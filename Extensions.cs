1f83c5361548795e762c15f00c4b0eb08077418d
Konrad Dąbrowski <konrad.dabrowski.90@gmail.com>
2022-05-05 22:56:00 +0200

    Update to .NET 6

--- Extensions.cs
+++ Extensions.cs
@@ -2,18 +2,17 @@ using Arcaim.DI.Scanner;
 using FluentValidation;
 using Microsoft.Extensions.DependencyInjection;
 
-namespace Arcaim.Assertor
-{
-    public static class Extensions
-    {
-        public static IServiceCollection AddAssertor(this IServiceCollection services)
-        {
-            services.AddSingleton<Arcaim.CQRS.WebApi.Interfaces.IValidator, Validator>();
-            services.Scan(a => a.ByAppAssemblies()
-                .InheritedFrom(typeof(AbstractValidator<>))
-                .WithTransientLifetime());
+namespace Arcaim.Assertor;
 
-            return services;
-        }
-    }
+public static class Extensions
+{
+  public static IServiceCollection AddAssertor(this IServiceCollection services)
+  {
+    services.AddSingleton<Arcaim.CQRS.WebApi.Interfaces.IValidator, Validator>();
+    services.Scan(a => a.ByAppAssemblies()
+      .ImplementationOf(typeof(AbstractValidator<>))
+      .WithTransientLifetime());
+
+    return services;
+  }
 }
--- Validator.cs
+++ Validator.cs
@@ -1,26 +1,23 @@
 using System.Threading.Tasks;
-using Arcaim.Assertor.Exceptions;
 using FluentValidation;
 using Microsoft.Extensions.DependencyInjection;
 
-namespace Arcaim.Assertor
+namespace Arcaim.Assertor;
+
+public class Validator : Arcaim.CQRS.WebApi.Interfaces.IValidator
 {
-    public class Validator : Arcaim.CQRS.WebApi.Interfaces.IValidator
+  private readonly IServiceScopeFactory _serviceFactory;
+
+  public Validator(IServiceScopeFactory serviceFactory)
+    => _serviceFactory = serviceFactory;
+
+  public async Task ValidateAsync<T>(T instance)
+  {
+    using var scope = _serviceFactory.CreateScope();
+    var validator = scope.ServiceProvider.GetService<AbstractValidator<T>>();
+    if (validator is not null)
     {
-        private readonly IServiceScopeFactory _serviceFactory;
-
-        public Validator(IServiceScopeFactory serviceFactory)
-            => _serviceFactory = serviceFactory;
-
-        public async Task ValidateAsync<T>(T instance)
-        {
-            using var scope = _serviceFactory.CreateScope();
-            var validator = scope.ServiceProvider.GetService<AbstractValidator<T>>();
-            if (validator is null)
-            {
-                throw AssertorServiceException.Create(nameof(AbstractValidator<T>));
-            }
-            await validator.ValidateAndThrowAsync(instance);
-        }
+      await validator.ValidateAsync(instance);
     }
+  }
 }
